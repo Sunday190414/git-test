1cdf70225e4b7d766507d81a8076abf0053c4844
yuhz <1966331954@qq.com>
2022-11-10 14:34:44 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author: yuhz
+ * @create: 2022/11/9
+ * @Description:
+ * @FileName: GitTest
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
