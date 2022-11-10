9a6e9e19c44bf7261735ba6bf02a72679b1a8454
yuhz <1966331954@qq.com>
2022-11-10 15:01:10 +0800

    hot-fix commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello,git world");
         System.out.println("hello,git world!");
+        System.out.println("hello,merge git");
     }
 }
